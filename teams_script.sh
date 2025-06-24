ef3854531fa6565900b4d5b035f829ef5be4b2ce
Ciberspring Jean Berrios <ciberspringjeanberrios@Jeans-MacBook-Pro.local>
2025-06-24 10:22:04 -0400

    modified script to run every 60s

--- teams_script.sh
+++ teams_script.sh
@@ -5,5 +5,5 @@ do
     osascript -e 'tell application "Microsoft Teams" to activate'
     osascript -e 'tell application "System Events" to keystroke "2" using {command down}'
     echo "Teams Status Refreshed"
-    sleep 300
+    sleep 60
 done
